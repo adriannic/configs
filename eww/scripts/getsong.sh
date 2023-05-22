ed799a201abafc54ac3b634998d08595825dd90e
adriannic <spamadriannic@gmail.com>
2023-05-22 17:17:47 +0200

    Fix some characters on song name breaking json format

--- eww/scripts/getsong.sh
+++ eww/scripts/getsong.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-playerctl metadata --format '{"artist":"{{artist}}","album":"{{album}}","title":"{{title}}","position":"{{duration(position)}}","duration":"{{duration(mpris:length)}}","time-left":"{{duration(mpris:length-position)}}","status":"{{status}}","volume":{{volume*100}},"player-name":"{{playerName}}"}' -F --player=%any --ignore-player=firefox
+playerctl metadata --format '{{status}} - {{artist}} - {{title}} {{duration(position)}}/{{duration(mpris:length)}}' -F --player=%any --ignore-player=firefox
